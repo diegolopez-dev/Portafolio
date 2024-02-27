15e40a884e31741fb4c90ec2e5d059d6078226fb
diegolopez-dev <diego.teide@gmail.com>
2024-02-27 01:25:23 +0100

    Agregando formulario

--- assets/js/app.js
+++ assets/js/app.js
@@ -59,3 +59,32 @@ window.addEventListener('load', function(){
 		]
 	});
 });
+
+// Envio de formulario
+
+const $form = document.querySelector('#form');
+
+$form.addEventListener('submit', handleSubmit);
+
+async function handleSubmit(event) {
+	event.preventDefault()
+	const form = new FormData(this)
+	const response = await fetch(this.action, {
+		method: this.method,
+		body: form, 
+		headers: {
+			'Accept': 'application/json'
+		}
+	})
+	if (response.ok) {
+		this.reset()
+		Swal.fire({
+			title: "¡GRACIAS!",
+			text: "Formulario enviado correctamente",
+			iconColor: '#ffff00',
+			color: '#ffff00',
+			background: '#0b0a13',
+			icon: "success"
+		  });
+	}
+}
